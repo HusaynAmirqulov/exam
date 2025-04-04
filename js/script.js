2f5a0914e1892dc4caa406f576a3e5c50010f7c5
HusaynAmirqulov <amirqulovhusayn@gmail.com>
2025-04-04 09:07:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
